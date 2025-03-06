12b0c9bac7c994edb6d9faa1407da760f65c1269
Jesiel Faria <fariajesiel@gmail.com>
2025-03-06 08:28:05 -0300

    Projeto inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redireciona para Login
+  { path: 'login', component: LoginComponent },
+  // { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
